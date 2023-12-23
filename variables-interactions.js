14b28180af27e813ed367db188ac47bc7f347a72
Bikash <sahb2676@gmail.com>
2023-12-23 16:34:52 +0545

    Leared about variables and interactions

---
+++ variables-interactions.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+// ES5
+var person = "Bikash";
+person = "hari";
+console.log(person);
+
+// ES6
+// let allowa us to reassign the variable
+let person1 = "Anish";
+person1 = "Manoj";
+console.log(person1);
+
+// const refers to constant so it doesn't allow us to change the value of the variable after once assigned.
+const num = 5;
+// num = 9;
+console.log(num);
+
+// Alert gives up a pop up when we load the site..
+alert("Hello This is alert");
+
+// Prompt is used to take any input from the user.
+const form1 = prompt("What is your age?");
+
+// Confirm is used to have yes/no
+const form2 = confirm("Are u Happy rn?");
+console.log(form1, form2);
