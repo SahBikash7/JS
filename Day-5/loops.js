3a9f8d2113d68cae884c8cd3849bb03ea5824b19
Bikash <sahb2676@gmail.com>
2023-12-23 17:07:24 +0545

    Learned about loops

---
+++ Day-5/loops.js
@@ -0,0 +1,24 @@
+// Write a loop that prints from 1 to 100.
+
+// Using for loop:
+const num = Number(
+  prompt("Enter the number of which u want the multiplication table:")
+);
+for (i = 1; i <= 10; i++) {
+  console.log(num + " * " + i + " = " + num * i);
+}
+
+// Using While loop:
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
+// }
+
+// Using do While:
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 100);
